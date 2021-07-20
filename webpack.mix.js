95a9e9e492f35398ba0a618e684c3b98c5abbb73
medialogo <ikemi@medialogo.com>
2021-07-21 02:33:10 +0900

    back to tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+ mix.js('resources/js/app.js', 'public/js')
+ .postCss('resources/css/app.css', 'public/css', [
+     require('tailwindcss'),
+ ]);
